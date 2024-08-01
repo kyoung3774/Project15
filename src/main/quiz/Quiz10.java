083284af145aca798d065acac9bcd0066b26f9e3
BaeJooHwan <kyoung3774@naver.com>
2024-08-01 12:03:42 +0900

    2024.08.01

--- src/main/quiz/Quiz10.java
+++ src/main/quiz/Quiz10.java
@@ -4,19 +4,19 @@ public class Quiz10 {
 
 	public static void main(String[] args) {
 		Factorial factorial = new Factorial();
-		System.out.println(factorial.factorial(5));
+		System.out.println(factorial.factorial(3));
 	}
 
 }
 
 class Factorial {
-	// 재귀함수로 factorial 구하기
+	// 재귀함수로 factorial 구하기	
 	public static int factorial(int num) {
 		if (num == 1) {
 			return 1;
 		} else {
 			System.out.println("factorial(" + num + ")"); // 몇번 호출했는지 확인해보죠
 		return num * factorial(num - 1);
-		}
+		}	
 	}
 }
